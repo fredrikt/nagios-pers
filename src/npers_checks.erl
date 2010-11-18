2b0870b0a1245e58990d9d56b50fb572be061dd3
Fredrik Thulin <ft@it.su.se>
2010-11-18 22:10:47 +0100

    Shuffle checks list.

--- src/npers_checks.erl
+++ src/npers_checks.erl
@@ -14,4 +14,18 @@
 get_all(Options) ->
     Filename = proplists:get_value(erl_checks_filename, Options),
     {ok, [CheckList]} = file:consult(Filename),
-    {ok, CheckList}.
+    %% Shuffle result to achieve at least a primitive form of what
+    %% Nagios documentation calls 'interleaving'. Meaning don't hit
+    %% all services on one host at the exact same time.
+    {ok, shuffle(CheckList)}.
+
+%% Fisher-Yates shuffle from
+%% http://en.literateprograms.org/Fisher-Yates_shuffle_%28Erlang%29
+shuffle(L) ->
+    shuffle(L, []).
+
+shuffle([], Acc) ->
+     Acc;
+shuffle(List, Acc) ->
+    {Leading, [H | T]} = lists:split(random:uniform(length(List)) - 1, List),
+    shuffle(Leading ++ T, [H | Acc]).
